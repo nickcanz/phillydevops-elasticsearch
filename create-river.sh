73c2b1bb2341d71730ff3f8904fbe5ecc40a80d4
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2014-03-09 23:04:37 -0400

    Fixing scripts

--- create-river.sh
+++ create-river.sh
@@ -1,14 +1,16 @@
+curl -XDELETE http://localhost:9200/_river/phillydevops
 curl -XPOST http://localhost:9200/_river/phillydevops/_meta -d '{
   "type": "couchdb",
   "couchdb": {
-    "host" : "172.17.0.3",
+    "host" : "172.17.0.7",
     "port" : 5984,
-    "db" : "phillydevops"
+    "db" : "phillydevops",
+    "filter": null
   },
   "index" : {
     "index" : "phillydevops",
     "type" : "meeting",
-    "bulk_size" : "1",
-    "bulk_timeout" : "10ms"
+    "bulk_size" : "10",
+    "bulk_timeout" : "100ms"
   }
 }'
--- elasticsearch-dockerfile/create-index.sh
+++ elasticsearch-dockerfile/create-index.sh
@@ -2,15 +2,12 @@ curl -XPUT 'http://localhost:9200/phillydevops/' -d '{
   "settings" : {
     "index": {
       "number_of_shards": 5,
-      "number_of_replicas": 2
+      "number_of_replicas": 1
     }
   },
   "mappings" : {
     "meeting" : {
-      "properties" : {
-        "title" : { "type" : "string", "index": "analyzed" },
-        "date" : { "type": "date", "index": "analyzed" }
-      }
+      "properties" : { }
     }
   }
 }'
